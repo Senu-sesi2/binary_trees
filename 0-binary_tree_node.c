f4427a704de6f2f2cb9b5da74b53ba016843c5e3
Senu-sesi2 <senu.sesi2@gmail.com>
2024-02-29 17:09:24 +0000

    a function that inserts a node as the left-child of another node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 /**
- * binary_tree_node - creates a binary tree node
+ * binary_tree_node - that creates a binary tree node
  *
  * @parent: pointer to the parent node
  * @value: value to put in the node
